b1d7e7570194d91a5ba246db914d6c77b0dc9afd
Nguyen Van Tien <nguyenvantien1122005@gmail.com>
2025-06-04 16:37:59 +0700

    sua loi

--- script/DangKy_DangNhap.js
+++ script/DangKy_DangNhap.js
@@ -1,32 +1,25 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
 
-
-
-
-function login(e){
-	event.preventDefault();
-	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
-	if(email == null){
-	  alert("Vui lòng nhập email và mật khẩu")
-	}
-	else if(email == DataTransfer.email && password == DataTransfer.password){
-	  alert("Đăng nhập thất bại!!")
-	}
-	else{
-	  alert("Đăng nhập thành công!1")
-	  window.location.href="index.html"
-	}
+function login(e) {
+  e.preventDefault();
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  if (email == null) {
+    alert("Vui lòng nhập email và mật khẩu");
+  } else if (email == DataTransfer.email && password == DataTransfer.password) {
+    alert("Đăng nhập thất bại!!");
+  } else {
+    alert("Đăng nhập thành công!1");
+    window.location.href = "index.html";
   }
-
-
+}
